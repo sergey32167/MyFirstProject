e4c5c5090d8efb34105c84037d59487585fc6a2c
sergey32167 <serg-li-@mail.ru>
2021-05-02 12:01:02 +0300

    Домашнее задание. одномерные 8 из 9. многомерные 4 из 5.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,7 @@ package com.company;
 
 public class Main {
 
-       public static void main(String[] args) {
-    System.out.println("Hello World");
-    lesson2.task2();
-    lesson2.task3();
-    lesson2.task4();
-    lesson2.task5();
-    lesson2.task6();
-    lesson2.task7();
-       }
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+    }
 }
