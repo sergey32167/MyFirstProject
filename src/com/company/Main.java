51d0fa7a9f6b7723819c202fdb6fdb5002419365
sergey32167 <serg-li-@mail.ru>
2021-05-03 16:23:25 +0300

    с исправлениями 5 и 7 задания

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args)
     { System.out.println("Hello World");
         Lesson3 Lesson3  = new Lesson3();
-        Lesson3.multitask4();
+        Lesson3.task6();
 
 
     }
