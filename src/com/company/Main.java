734fc14c93d1e9593c4cf527629d6255d67470ce
sergey32167 <serg-li-@mail.ru>
2021-05-03 16:23:25 +0300

    с исправлениями 5 и 7 задания

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	System.out.println("Hello World");
+    public static void main(String[] args)
+    { System.out.println("Hello World");
+        Lesson3 Lesson3  = new Lesson3();
+        Lesson3.multitask4();
+
+
     }
 }
