b9e795a83999fdccb7b6c09ea59f533273fcdc49
sergey32167 <serg-li-@mail.ru>
2021-05-07 10:40:58 +0300

    готовый компьютер

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,6 @@ public class Main {
 
        public static void main(String[] args)
     { System.out.println("Hello World");
-        Computer comp = new Computer();
-        comp.setRam(8);
-        comp.setRom(500);
-        comp.setPros("intel");
-        comp.print();
-        comp.turnOn();
-        comp.turnOff();
+
     }
 }
