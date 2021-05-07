edcd63ba91b4fd65b78c63cf7f955cb89a77228d
sergey32167 <serg-li-@mail.ru>
2021-05-07 10:40:58 +0300

    готовый компьютер

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,9 @@
 package com.company;
 
-import com.company.lesson5.CivilTransport;
-import com.company.lesson5.MilitaryTransport;
-
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-
-        CivilTransport transport = new CivilTransport(10, true, "sfdsf",11,11,11,11,11);
-        transport.info();
-        transport.man();
-        transport.check();
+       public static void main(String[] args)
+    { System.out.println("Hello World");
 
     }
 }
