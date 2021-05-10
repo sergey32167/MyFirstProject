c8194f9699c2b37484ed9acc2f583f4c17a6f23a
sergey32167 <serg-li-@mail.ru>
2021-05-10 23:46:33 +0300

    домашка 5 урок

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,18 @@ package com.company;
 
 public class Main {
 
-       public static void main(String[] args)
-    { System.out.println("Hello World");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        LightTransport transport = new LightTransport();
+        transport.setBrand("Audi");
+        transport.setMass(1900);
+        transport.setMaxspeed(250);
+        transport.setPower(216);
+        transport.setPeople(4);
+        transport.setBody("купе");
+        transport.setFuel(5.5);
+        transport.setWheel(4);
 
     }
 }
