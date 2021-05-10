8e329f7c717e7de69031da6a3ad6e26c47bf7485
sergey32167 <serg-li-@mail.ru>
2021-05-10 23:46:33 +0300

    домашка 5 урок

---
+++ src/com/company/AirTransport.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class AirTransport extends Transport {
+
+    public int wings;
+    public int band;
+
+    public void setWings(int wings) {
+        this.wings = wings;
+    }
+    public void setBand(int band) {
+        this.band = band;
+    }
+
+}
--- src/com/company/Transport.java
+++ src/com/company/Transport.java
@@ -1,5 +1,37 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Transport {
+
+    public int power;
+    public int maxspeed;
+    public int mass;
+    public String brand;
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public void setMaxspeed(int maxspeed) {
+        this.maxspeed = maxspeed;
+    }
+
+    public void setMass(int mass) {
+        this.mass = mass;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void horse() {
+
+
+        double a;
+        a = power * 0.74;
+        System.out.println(a + "Кв");
+
+    }
 
 }
